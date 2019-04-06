24046a1dc6c1361f1b63a7e591a31b206d3f8939
Gary Liang <gary@arkade.com.au>
2019-04-06 13:51:05 +1100

    style

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import Config from '../../config';
 
 const Home = ({ data }) => {
   function buildLayout() {
+    const { deleteJobUrl, singleJobUrl } = Config;
+
     let res = data.map((item, index) => {
       return (
         <tr key={index}>
-          <td><a href="#">{item.title}</a></td>
-          <td><a href="#">Delete</a></td>
+          <td><a href="#" onClick={() => this.viewSingleJob(item.id)}>{item.title}</a></td>
+          <td><a href="#" onClick={() => this.deleteSingleJob(item.id)}>Delete</a></td>
         </tr>
       );
     });
 
     return res;
+  }
+
+  function viewSingleJob(id) {
+    console.log(id);
+  }
+
+  function deleteSingleJob(id) {
+    console.log(id);
   }
 
   return (
