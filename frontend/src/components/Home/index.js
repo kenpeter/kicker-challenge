ba88b135ccbf599be5774c4de157419f5291bf30
Gary Liang <gary@arkade.com.au>
2019-04-06 13:32:10 +1100

    add bootstrap

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 const Home = ({ data }) => {
   function buildLayout() {
-    console.log('--');
-    console.log(data);
+    let res = data.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td><a href="#">{item.title}</a></td>
+          <td><a href="#">Delete</a></td>
+        </tr>
+      );
+    });
+
+    return res;
   }
 
   return (
     <div className='home'>
-      { buildLayout() }
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Job title</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          { buildLayout() }
+        </tbody>
+      </table>
     </div>
   );
 };
